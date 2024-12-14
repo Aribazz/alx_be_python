c5ec7e3b9f1770d615e3cae98f1e1debae9abea6
Ariyo <ariyoaribass68@gmail.com>
2024-12-14 07:47:40 +0100

    Add basic_operations.py with basic python operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = int("10")
+number2 = int("5")
+
+sum_result = number1 + number2
+diffference_result = number1 - number2
+product_result = number1 * number2 
+
+print(f"Addition {number1} and {number2} is {sum_result}")
+print (f"Substraction {number1} and {number2} is {diffference_result}")
+print(f"Multiplication {number1} and {number2} is {product_result}")
