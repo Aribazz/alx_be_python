95e25fe949c9f9f0d014d70b615d1b69dccdeb27
Ariyo <ariyoaribass68@gmail.com>
2024-12-23 06:31:24 +0100

    first weather control

--- future_age_calculator.py
+++
@@ -1,4 +0,0 @@
-current_age = int(input("How old are you?"))
-age_in_2050 = 2050 - 2020
-sum_current = age_in_2050 + current_age
-print(f"In 2050, you will be {sum_current} years old.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,3 +2,4 @@ hours = 2
 seconds = hours * 3600 
 
 print(f"{hours} hours is {seconds} seconds. ")
+
